12703f45ef5a8cba126fec41a6f95103b74a5c5c
Mrunali1004 <zalavadiyamrunali@gmail.com>
2024-04-27 13:27:40 -0400

    Add JWT authentication middleware and category creation endpoint

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 8080;
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const noteRoutes = require("./routes/notesRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const { authenticateToken } = require("./middleware/authenticateToken");
 
 app.use(
   cors({
@@ -12,13 +14,12 @@ app.use(
   })
 );
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Routes
 app.use("/", authRoutes);
-app.use("/notes", noteRoutes);
+app.use("/notes", authenticateToken, noteRoutes);
+app.use("/category", authenticateToken, categoryRoutes);
 
 app.listen(PORT, () => {
   console.log(`welcom to my ${PORT} `);
